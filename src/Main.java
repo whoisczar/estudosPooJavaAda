b7a7e5c38b93f2387cd54d58f68e3d2dc92d2245
Gabriel <gabi.reis200@gmail.com>
2024-06-15 22:40:57 -0300

    First Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
+        ArrayList abc = new ArrayList();
         System.out.println("Hello world!");
     }
 }
